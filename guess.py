15e79567e389a111a609af67a10a818131952884
Sardor Abdujabborov <com.scorp.dev@gmail.com>
2025-11-25 20:08:27 +0500

    limit number of guesses

--- guess.py
+++ guess.py
@@ -2,9 +2,15 @@ import random
 
 n = random.randint(0,10)
 guess = int(input("guess a number between 0 and 10: "))
+guesses = 0
+
 while True:
     if n == guess:
         print("True !")
         break
+    if guesses >= 3:
+        print("Yutqazdingiz!!!")
+        break
+    guesses +=1
 
 
